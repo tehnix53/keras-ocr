3f72e3027e3efc9979cfd9ddb0c51c19c91d8500
Alexander <44552657+tehnix53@users.noreply.github.com>
2021-04-22 17:02:27 +0200

    Update tools.py

--- keras_ocr/tools.py
+++ keras_ocr/tools.py
@@ -955,7 +955,8 @@ def got_statistic_word(pred, obj):
     содержащую все не расознанные или ошибочно\
     распознанные символы"    
     
-   def _compare_words(g_true, predict):
+    
+    def _compare_words(g_true, predict):
 
         "находит не распознанные \
         или ошибочно распознанные символы\
@@ -967,7 +968,7 @@ def got_statistic_word(pred, obj):
                 answer += i
         return answer
     
-   def _compare_difference(number_a, number_b):
+    def _compare_difference(number_a, number_b):
 
         "сравнивает координаты для\
         определения наложения точек"
@@ -979,8 +980,8 @@ def got_statistic_word(pred, obj):
             return True
         else:
             return False 
-
-   def _compare_coordinate(a,b,i,j):
+        
+    def _compare_coordinate(a,b,i,j):
 
         "сравнивает 4 координаты диагонали\
             ректов"
